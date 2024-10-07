fe935dcf3aa1b0dae8679598edd95086f9b1368a
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-10-07 14:17:02 +0530

    🌹

---
+++ Telegram-Bots/Web-App.py
@@ -0,0 +1,8 @@
+from pyrogram.types import WebAppInfo 
+
+#=== InlineKeyboardButton Code ===#
+
+
+InlineKeyboardButton('🧿 Wᴀᴛᴄʜ ᴏɴ ᴛᴇʟᴇɢʀᴀᴍ 🖥', web_app=WebAppInfo(url=web_link))#Don not remove credit else gay 
+
+#Add It On Stream Callback_data Code and Don't Forgot To Change `web_link` with appropriate variables according to repo..
